9f2dd793d46f084c3c7a5efafe5f927a3d60dd31
CalderH <caldertheperson@me.com>
2023-11-13 19:47:16 -0500

    5

--- make_collage_shader.py
+++ make_collage_shader.py
@@ -4,5 +4,20 @@ from pathlib import Path
 code_path = str(Path(workspace(q=True, rd=True)) / 'shading' / 'fantasy' / 'code')
 if code_path not in sys.path:
     sys.path.append(code_path)
+
+import importlib
+from internals import network, shading_path, shading_controller, surface_values, coordinate_converter, palettes, collage_shader, world_placement, screen_placement, tracking_projection, dialog_with_support
+importlib.reload(network)
+importlib.reload(shading_path)
+importlib.reload(shading_controller)
+importlib.reload(collage_shader)
+importlib.reload(tracking_projection)
+importlib.reload(coordinate_converter)
+importlib.reload(surface_values)
+importlib.reload(palettes)
+importlib.reload(world_placement)
+importlib.reload(screen_placement)
+importlib.reload(dialog_with_support)
+
 from internals import make_collage_shader
 make_collage_shader.run()
