19701459a57ff0ae3915796d5a289530ff5a77e8
CalderH <caldertheperson@me.com>
2023-11-13 19:35:25 -0500

    try 3

--- internals/make_collage_shader.py
+++ internals/make_collage_shader.py
@@ -21,6 +21,13 @@ from internals.collage_shader import CollageShader
 from pathlib import Path
 from shutil import rmtree
 import json
+from enum import Enum
+
+
+class MapDataStatus(Enum):
+    nonexistent = 0
+    up_to_date = 1
+    out_of_date = 2
 
 
 def run():
@@ -44,16 +51,16 @@ def run():
         with map_data_path.open() as file:
             original_map_data = json.load(file)
         if original_map_data['last modified'] == image_modification_time:
-            map_data_status = 'up to date'
+            map_data_status = MapDataStatus.up_to_date
         else:
-            map_data_status = 'out of date'
+            map_data_status = MapDataStatus.out_of_date
     else:
-        map_data_status = 'nonexistent'
+        map_data_status = MapDataStatus.nonexistent
 
     if not map_dir_path.exists():
         map_dir_path.mkdir()
 
-    if map_data_status != 'up to date':
+    if map_data_status != MapDataStatus.up_to_date:
         map_proc = subprocess.run(['python3', shading_path('code', 'internals', 'map_data.py'), map_image_path, map_data_path], capture_output=True)
         if map_proc.returncode or (err := map_proc.stderr.decode("utf-8")) != '':
             err = map_proc.stderr.decode("utf-8")
@@ -63,33 +70,33 @@ def run():
                 title = 'Error'
                 msg = err
             dialog_output = dialog_with_support(title, msg, ['I’ll fix it'], cb='I’ll fix it', db='I’ll fix it', icon='warning')
-            if map_data_status == 'nonexistent':
+            if map_data_status == MapDataStatus.nonexistent:
                 map_data_path.unlink(missing_ok=True)
             exit()
 
         with map_data_path.open() as file:
-            map_data = json.load(file)
+            new_map_data = json.load(file)
 
-        map_data['last modified'] = image_modification_time
+        new_map_data['last modified'] = image_modification_time
 
-        if map_data['anti-aliasing warning']:
+        if new_map_data['anti-aliasing warning']:
             msg = 'It looks like you may have used a brush with anti-aliasing, which produces many colors along the edge of a stroke. If that is the case and you make a shader based on this map, each of those different colors along the edge will become its own facet. Do you still want to continue?'
             dialog_output = dialog_with_support('Wait', msg, ['No', 'Yes'], cb='No', db='Yes', icon='question')
             if dialog_output in ['No', 'dismiss']:
                 map_data_path.unlink()
                 exit()
             else:
-                map_data['anti-aliasing warning'] = False
+                new_map_data['anti-aliasing warning'] = False
 
-    facet_borders_changed = map_data_status == 'nonexistent' or (map_data_status == 'out of date' and (('pixels' in original_map_data != 'pixels' in map_data) or ('pixels' in original_map_data and 'pixels' in map_data and original_map_data['pixels'] != map_data['pixels'])))
-    blur_markers_changed = map_data_status == 'nonexistent' or (map_data_status == 'out of date' and [facet['blur markers'] for facet in original_map_data['facets'].values()] != [facet['blur markers'] for facet in map_data['facets'].values()])
+    facet_borders_changed = map_data_status == MapDataStatus.nonexistent or (map_data_status == MapDataStatus.out_of_date and (('pixels' in original_map_data != 'pixels' in new_map_data) or ('pixels' in original_map_data and 'pixels' in new_map_data and original_map_data['pixels'] != new_map_data['pixels'])))
+    blur_markers_changed = map_data_status == MapDataStatus.nonexistent or (map_data_status == MapDataStatus.out_of_date and [facet['blur markers'] for facet in original_map_data['facets'].values()] != [facet['blur markers'] for facet in new_map_data['facets'].values()])
 
     masks_exist = masks_path.exists() and any(file.suffix == '.png' for file in masks_path.iterdir())
 
-    if map_data_status == 'up to date':
+    if map_data_status == MapDataStatus.up_to_date:
         num_facets = len(original_map_data['facets'])
     else:
-        num_facets = len(map_data['facets'])
+        num_facets = len(new_map_data['facets'])
     if num_facets > 1 and (not masks_exist or facet_borders_changed or blur_markers_changed):
         if masks_exist:
             rmtree(masks_path, ignore_errors=True)
@@ -114,9 +121,9 @@ def run():
         surface_values_path = calculate_surface_values(obj, map_data_path, blur_resolution)
         blur_proc = subprocess.run(['python3', shading_path('code', 'internals', 'blur_images.py'), surface_values_path, map_data_path], capture_output=True)
 
-    if map_data_status != 'up to date':
+    if map_data_status != MapDataStatus.up_to_date:
         with map_data_path.open('w') as file:
-            json.dump(map_data, file, indent=4)
+            json.dump(new_map_data, file, indent=4)
 
     for node in selection:
         if node.type() == 'mesh':
