b257c329176f0a2cd740de2466c028d6a9e69dd2
CalderH <caldertheperson@me.com>
2023-11-20 13:13:05 -0800

    updated unique name

--- internals/unique_name.py
+++ internals/unique_name.py
@@ -2,8 +2,4 @@ from pymel.core import *
 
 
 def unique_name(obj):
-    parents = [obj]
-    while (next_parent := parents[0].getParent()) is not None:
-        parents.insert(0, next_parent)
-    names = [item.name() for item in parents]
-    return '_'.join(names)
+    return obj.name().replace('|', '_')
