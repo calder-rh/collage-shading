dbf5db2107790eab798a8177e662e0b33e415ab3
CalderH <caldertheperson@me.com>
2024-03-19 15:03:02 -0400

    Update system_check.py

--- button_source/system_check.py
+++ button_source/system_check.py
@@ -2,7 +2,7 @@ from pymel.core import *
 from internals import network
 
 def run():
-    confirmDialog(t='Success', m='Last updated Friday March 8 at 11pm', b=['OK'], ma='left')
+    confirmDialog(t='Success', m='Last updated Tuesday March 19 at 3pm', b=['OK'], ma='left')
 
 def error(msg):
     confirmDialog(t='Error', m=msg, b=['OK'], ma='left')
