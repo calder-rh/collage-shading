4dfe4d92d465dcee3c933d99befb3e49d4449548
CalderH <caldertheperson@me.com>
2024-03-20 00:21:55 -0400

    debug ground 1

--- internals/ground.py
+++ internals/ground.py
@@ -89,6 +89,6 @@ class GroundShader(Network):
         gcn.other_internals.light_dot >> light_dot_remap.inputValue
         light_dot_remap.message >> obj_transform.angle_remap
 
-        weighted_luminance = self.multiply(gcn.luminance, obj_transform.luminance_weight)
+        weighted_luminance = self.multiply(gcn.luminance, obj_transform.luminance_weight, 'weighted_luminance')
         lightness = self.add(light_dot_remap.outValue, weighted_luminance, 'lightness')
         lightness >> obj_transform.lightness
